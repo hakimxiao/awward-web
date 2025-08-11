3ad76471c99e2b72dd9179b670ab5f7aa87a778d
abbu solihin alhakim <alhakimabusolihin@gmail.com>
2025-08-11 22:27:59 +0700

    Membuat component story yang mengimplementasikan AnimatedTitle dan img Dengan mouse feed || Ini masih belum selesai ya

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { useRef, useState } from "react";
 import { TiLocationArrow } from "react-icons/ti";
 
---
+++ src/components/Story.jsx
@@ -0,0 +1,80 @@
+import { useRef } from "react";
+import AnimatedTitle from "./AnimatedTitle";
+import gsap from "gsap";
+
+const Story = () => {
+  const frameRef = useRef("null");
+
+  const handleMouseLeave = () => {
+    const element = frameRef.current;
+
+    gsap.to(element, {
+      duration: 0.3,
+      rotateX: 0,
+      rotateY: 0,
+      ease: "power1.inOut",
+    });
+  };
+
+  const handleMouseMove = (e) => {
+    const { clientX, clientY } = e;
+    const element = frameRef.current;
+
+    if (!element) return;
+
+    const rect = element.getBoundingClientRect();
+    const x = clientX - rect.left;
+    const y = clientY - rect.top;
+
+    const centerX = rect.width / 2;
+    const centerY = rect.height / 2;
+
+    const rotateX = ((y - centerX) / centerY) * -10;
+    const rotateY = ((x - centerX) / centerX) * 10;
+
+    gsap.to(element, {
+      duration: 0.3,
+      rotateX,
+      rotateY,
+      transformPerspective: 500,
+      ease: "power1.inOut",
+    });
+  };
+
+  return (
+    <section id="story" className="min-h-dvh w-screen bg-black text-blue-50">
+      <div className="flex size-full flex-col items-center py-10 pb-24">
+        <p className="font-general text-sm uppercase md:text-[10px]">
+          the emultiversal ip world
+        </p>
+
+        <div className="relative size-full">
+          <AnimatedTitle
+            title="The st<b>o</b>ry of a hidden real<b>m</b>"
+            sectionId="#story"
+            containerClass="mt-5 pointer-events-none mix-blend-difference relative z-10"
+          />
+
+          <div className="story-img-container">
+            <div className="story-img-mask">
+              <div className="story-img-content">
+                <img
+                  ref={frameRef}
+                  src="/img/entrance.webp"
+                  alt="enterance"
+                  className="object-contain"
+                  onMouseLeave={handleMouseLeave}
+                  onMouseUp={handleMouseLeave}
+                  onMouseEnter={handleMouseLeave}
+                  onMouseMove={handleMouseMove}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Story;
